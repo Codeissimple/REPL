fe638f9bfbe4921e13f100cf6ce07daf849681d8
Codeissimple <>
2021-04-30 00:03:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+const int ARRAY_SIZE = 5;
+
+int main() {
+  double grades[ARRAY_SIZE] = { 25, 50, 75, 100, 0};
+  double grades1[ARRAY_SIZE] = {25, 50, 78, 99, 34};
+  double accumulator = 0.0;
+  double average = 0.0;
+
+for (int i = 0; i < ARRAY_SIZE; i++)
+  accumulator = accumulator + grades[i];
+
+average = accumulator / ARRAY_SIZE;
+
+for (int i = 0; i < ARRAY_SIZE; i ++)
+  if (grades[i] != grades1[i])
+    cout << "The elements numbered " << i + 1 << " are not equal\n";
+  else
+    cout << "The elements numbered " << i + 1 << " are equal\n"; 
+
+
+cout << "\nThe average of grades is " << average << endl;
+cout << "The size of the array is " << ARRAY_SIZE << endl;
+}
